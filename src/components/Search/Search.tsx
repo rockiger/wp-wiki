4e5b005ae401ac600e92afa9a6ae41694bcf7130
rockiger <marco@rockiger.com>
2023-06-17 23:23:35 +0200

    Add dark mode for search.

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -11,7 +11,6 @@ import CloseIcon from 'mdi-react/CloseIcon'
 import { Page } from '../../api'
 import IconButton from '../IconButton'
 
-//! darkmode
 //! provide classes for customization with tailwind
 //! provide functions for behavior customization
 //! tests
